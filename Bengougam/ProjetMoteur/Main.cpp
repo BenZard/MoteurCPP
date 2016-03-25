e3a800ab8705b4cc7ce71f7374d1490540514d44
Benoit Olivier <benoit.oliviermuzard@gmail.com>
2016-03-25 14:20:52 +0100

    no message

--- Bengougam/ProjetMoteur/Main.cpp
+++ Bengougam/ProjetMoteur/Main.cpp
@@ -1,17 +1,23 @@
 // ProjetMoteur.cpp : définit le point d'entrée pour l'application console.
 //
+#include "stdafx.h"
+#include <iostream>
 #include <cstdlib>
 #include <cstdio>
 #include <cstdint>
-#include "Common/EsgiTimer.h"
-#include "stdafx.h"
-#include <iostream>
+#include <functional>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
 #include <stdio.h>
 #include <list>
 #include <map>
+#include "assert.h"
+#include "Common/EsgiTimer.h"
 
 using namespace std;
-
 struct Vector3
 {
 public:
@@ -35,7 +41,7 @@ protected: // Classe abstraite pour les messages : un destinataire, un type de m
 		, m_messageType(messageTypeID)
 	{}
 
-public: 
+public:
 	int m_targetGameObject;
 	eMsgGameObj m_messageType;
 };
@@ -71,13 +77,31 @@ public:
 	{}
 };
 
-class CoreComponent
+// Component peut accéder à son parent. 
+class Component
 {
+private:
+	int m_ComponentID;
+	static int LastComponentID;
 public:
+	Component()
+	{
+		m_ComponentID = LastComponentID;
+		LastComponentID++;
+	}
+
+
 	virtual bool SendMessage(CoreMessage* msg) { return false; }
+
+	virtual void update(float dt)
+	{
+		std::cout << m_ComponentID << std::endl;
+	}
+
 };
 
-class RenderComponent : public CoreComponent
+
+class RenderComponent : public Component
 {
 public:
 	bool SendMessage(CoreMessage* msg)
@@ -90,19 +114,71 @@ public:
 		}
 		else
 		{
-			return CoreComponent::SendMessage(msg); // False
+			return Component::SendMessage(msg); // False
 		}
 	}
 };
 
+// fonction qui renvoie le component at un index. 
+
 class GameObject
 {
+
+private:
+	static int LastGameObjectID;
+	std::vector<Component*> m_Components;
+	Vector3 m_Position;
+	Vector3 m_Velocity;
+	Vector3 m_BoundingBox;  // dx,dy,dz 
+	bool PassMessageToComponents(CoreMessage* msg)
+	{
+		bool isSent = false;
+
+		std::vector<Component*>::iterator compIt = m_Components.begin();
+		for (compIt; compIt != m_Components.end(); ++compIt)
+		{
+			isSent |= (*compIt)->SendMessage(msg);
+		}
+
+		return isSent;
+	}
+
+protected:
+	int m_GameObjectID;
+
 public:
-	GameObject(int uniqueID): m_UniqueID(uniqueID){	}
 
-	int GetGameObjectID() const { return m_UniqueID; }
+	GameObject()
+	{
+		m_GameObjectID = LastGameObjectID;
+		LastGameObjectID++;
+	}
 
-	void AddComponent(CoreComponent* comp)
+	virtual void Update(float dt)
+	{
+		std::cout << m_GameObjectID << std::endl;
+	}
+
+	int GetGameObjectID() const { return m_GameObjectID; }
+
+	int GetComponentsSize()
+	{
+		return m_Components.size();
+	}
+	
+	Component * GetComponentAt(int index)
+	{
+		if (index < 0 || index >= m_Components.size())
+		{
+			return NULL;
+		}
+		else
+		{
+			return m_Components.at(index);
+		}
+	}
+
+	void AddComponent(Component* comp)
 	{
 		m_Components.push_back(comp);
 	}
@@ -146,90 +222,245 @@ public:
 		return isSent;
 	}
 
-private: 
-	int m_UniqueID;
-	std::list<CoreComponent*> m_Components;
-	Vector3 m_Position;
-	Vector3 m_Velocity;
-	Vector3 m_BoundingBox;  // dx,dy,dz 
- 
+};
 
-private:
-	bool PassMessageToComponents(CoreMessage* msg)
-	{
-		bool isSent = false;
-
-		std::list<CoreComponent*>::iterator compIt = m_Components.begin();
-		for (compIt; compIt != m_Components.end(); ++compIt)
-		{
-			isSent |= (*compIt)->SendMessage(msg);
-		}
-
-		return isSent;
-	}
+class Creator
+{
 
 
 };
 
-class SceneManager
+class Factory
 {
 public:
-	// Returns true if the GameObject or any components handled the message.
-	bool SendMessage(CoreMessage* msg)
-	{
-	//Recherche du GameObject par son ID. 
-		std::map<int, GameObject*>::iterator gameObjIt = m_GameObjects.find(msg->m_targetGameObject);
-		if (gameObjIt != m_GameObjects.end())
-		{
-			// On envoie le message au GameObject.
-			return gameObjIt->second->SendMessage(msg);
-		}
-
-		// Si aucun GameObject n'a été trouvé.
-		return false;
-	}
-
-	// Fonction de création de GameObject avec ID. 
-	GameObject* CreateGameObject()
-	{
-		// remplacer le new. 
-		GameObject* newObj = new GameObject(nextID++);
-		m_GameObjects[newObj->GetGameObjectID()] = newObj;
-
-		return newObj;
-	}
-
-private:
-	std::map<int, GameObject*> m_GameObjects;
-	static int nextID;
+	GameObject* CreateObject(std::string type);
+	void Register(std::string type, Creator *creator);
+	std::map<std::string, Creator* > m_Factory;
+	std::map < std::string, std::vector<GameObject*> > m_Reserve;
 };
 
-// Initialization du generateur. 
-int SceneManager::nextID = 0;
 
-int main()
+
+class GOIterator
 {
-	SceneManager sceneMgr;
-	// On créé un nouveau game object et le place dans la scène
-	GameObject* TestGameObject = sceneMgr.CreateGameObject();
 
-	// Creer le component (new à enlever plus rard) 
-	RenderComponent* renderer = new RenderComponent();
-	TestGameObject->AddComponent(renderer);
+	int indexObject;
+	int indexComponent;
+	std::vector<GameObject*> * objects;
+public:
 
-	// Test Set Pos
-	SetPositionMessage msgSetPos(TestGameObject->GetGameObjectID(), 1.0f, 2.0f, 3.0f);
-	sceneMgr.SendMessage(&msgSetPos);
-	cout << "Position set to (1, 2, 3) on GameObject with ID: " << TestGameObject->GetGameObjectID() << '\n';
+	std::mutex mutex;
 
-	// Test Get pos
-	cout << "Retreiving position from GameObject with ID: " << TestGameObject->GetGameObjectID() << '\n';
-	GetPositionMessage msgGetPos(TestGameObject->GetGameObjectID());
-	sceneMgr.SendMessage(&msgGetPos);
-	cout << "X: " << msgGetPos.x << '\n';
-	cout << "Y: " << msgGetPos.y << '\n';
-	cout << "Z: " << msgGetPos.z << '\n';
+	GOIterator(std::vector<GameObject*>* obj) : objects(obj), mutex()
+	{
+		indexObject = 0;
+		indexComponent = 0;
+	}
+	/*
+	GameObject* get()
+	{
+		if (finished())
+		{
+			return NULL;
+		}
 
-	system("Pause");
+		return (*objects)[index];
+	}
+	*/
+	Component* getComponent()
+	{
+		if (finished())
+		{
+			return NULL;
+		}
 
+		while (indexComponent >= (*objects)[indexObject]->GetComponentsSize())
+		{
+			indexObject++;
+			indexComponent++;
+			if (finished())
+			{
+				return NULL;
+			}
+		}
+
+		return (*objects)[indexObject]->GetComponentAt(indexComponent);
+	}
+
+	bool finished()
+	{
+		return indexObject >= objects->size();
+	}
+
+	void plusplus()
+	{
+		if (finished())
+		{
+			return;
+		}
+
+		indexComponent++;
+		if (indexComponent >= (*objects)[indexObject]->GetComponentsSize())
+		{
+			indexComponent = 0;
+			indexObject++;
+		}
+	}
+};
+// Ajouter components au game object au moment ou je le créé avec une méthode add component (envoyer un component *) 
+class Worker
+{
+	std::condition_variable condWork;
+	GOIterator *iterator;
+	std::atomic<bool> working;
+	std::thread thread;
+public:
+
+	Worker() : thread(&Worker::work, this)
+	{
+		working.store(false);
+		iterator = NULL;
+	}
+
+	void addWork(GOIterator *p_iterator)
+	{
+		if (working)
+		{
+			assert(false);
+		}
+
+		iterator = p_iterator;
+		working.store(true);
+		condWork.notify_all();
+	}
+
+	void waitForEndOfWork()
+	{
+		while (working)
+		{
+
+		}
+	}
+
+	void work()
+	{
+		while (1)
+		{
+
+			iterator = NULL;
+			std::mutex mutex;
+			working.store(false);
+
+			while (!working)
+			{
+				condWork.wait_for(std::unique_lock<std::mutex>(mutex), std::chrono::milliseconds(10));
+			}
+
+			while (1)
+			{
+				Component* comp = NULL;
+				{
+					std::lock_guard<std::mutex> lock(iterator->mutex);
+
+					if (iterator->finished())
+					{
+						break;
+					}
+
+					comp = iterator->getComponent();
+					(*iterator).plusplus();
+				}
+
+				if (comp != NULL)
+				{
+					comp->update(0.03);
+				}
+			}
+
+		}
+	}
+
+
+};
+
+
+class Boss
+{
+	int nb_worker;
+	Worker* workers;
+
+public:
+	Boss(int p_nb_worker = 8) : nb_worker(p_nb_worker)
+	{
+		workers = new Worker[nb_worker];
+	}
+
+	~Boss()
+	{
+		delete[] workers;
+	}
+
+	void work(GOIterator *iterator)
+	{
+
+		for (int i = 0; i < nb_worker; i++)
+		{
+			workers[i].addWork(iterator);
+		}
+
+		for (int i = 0; i < nb_worker; i++)
+		{
+			workers[i].waitForEndOfWork();
+		}
+	}
+};
+
+
+
+class GameEngine
+{
+
+private:
+	Boss boss;
+	std::vector<GameObject*> objects;
+public:
+
+	void AddObject(GameObject* obj)
+	{
+		objects.push_back(obj);
+	}
+
+	GameEngine()
+	{
+
+	}
+
+	void Update()
+	{
+		GOIterator iterator(&objects);
+		boss.work(&iterator);
+	}
+
+};
+
+int GameObject::LastGameObjectID = 0;
+int Component::LastComponentID = 0;
+int main(int argc, char* argv[])
+{
+	GameEngine engine;
+	
+	for (int i = 0; i < 100; i++)
+	{
+		RenderComponent* renderer = new RenderComponent();
+		GameObject * obj = new GameObject();
+		obj->AddComponent(renderer);
+		engine.AddObject(obj);
+	}
+
+	while (1)
+	{
+		engine.Update();
+	}
+
+	return 0;
 }
--- Bengougam/ThreadPool.cpp
+++ Bengougam/ThreadPool.cpp
@@ -14,32 +14,179 @@
 #include <mutex>
 #include <condition_variable>
 #include <atomic>
+#include <stdio.h>
+#include <list>
 #include <map>
 #include "Common/EsgiTimer.h"
 
+using namespace std;
+struct Vector3
+{
+public:
+	Vector3() : x(0.0f), y(0.0f), z(0.0f)
+	{}
 
+	float x, y, z;
+};
+
+enum eMsgGameObj
+{
+	SetPosition,
+	GetPosition,
+};
+
+class CoreMessage
+{
+protected: // Classe abstraite pour les messages : un destinataire, un type de message
+	CoreMessage(int targetID, eMsgGameObj messageTypeID)
+		: m_targetGameObject(targetID)
+		, m_messageType(messageTypeID)
+	{}
+
+public:
+	int m_targetGameObject;
+	eMsgGameObj m_messageType;
+};
+
+class PositionMessage : public CoreMessage
+{
+protected: // Classe mère poru les messages de type Get/Set position 
+	PositionMessage(int targetID, eMsgGameObj messageType,
+		float X = 0.0f, float Y = 0.0f, float Z = 0.0f)
+		: CoreMessage(targetID, messageType)
+		, x(X)
+		, y(Y)
+		, z(Z)
+	{}
+
+public:
+	float x, y, z;
+};
+
+class SetPositionMessage : public PositionMessage
+{
+public:
+	SetPositionMessage(int targetID, float X, float Y, float Z)
+		: PositionMessage(targetID, SetPosition, X, Y, Z)
+	{}
+};
+
+class GetPositionMessage : public PositionMessage
+{
+public:
+	GetPositionMessage(int targetID)
+		: PositionMessage(targetID, GetPosition)
+	{}
+};
+class Component
+{
+public:
+	virtual bool SendMessage(CoreMessage* msg) { return false; }
+};
+
+class RenderComponent : public Component
+{
+public:
+	bool SendMessage(CoreMessage* msg)
+	{
+		if (msg->m_messageType == SetPosition)
+		{
+			// Ici on pourra mettr eà jour la position du Mesh
+			cout << "RenderComponent handling SetPosition\n";
+			return true;
+		}
+		else
+		{
+			return Component::SendMessage(msg); // False
+		}
+	}
+};
 
 class GameObject
 {
 
 private:
 	static int LastGameObjectID;
+	std::list<Component*> m_Components;
+	Vector3 m_Position;
+	Vector3 m_Velocity;
+	Vector3 m_BoundingBox;  // dx,dy,dz 
+	bool PassMessageToComponents(CoreMessage* msg)
+	{
+		bool isSent = false;
+
+		std::list<Component*>::iterator compIt = m_Components.begin();
+		for (compIt; compIt != m_Components.end(); ++compIt)
+		{
+			isSent |= (*compIt)->SendMessage(msg);
+		}
+
+		return isSent;
+	}
 
 protected:
-	int id;
+	int m_GameObjectID;
 
 public:
 
 	GameObject()
 	{
-		id = LastGameObjectID;
+		m_GameObjectID = LastGameObjectID;
 		LastGameObjectID++;
 	}
 
 	virtual void Update(float dt)
 	{
-		std::cout << id << std::endl;
+		cout << m_GameObjectID << endl;
 	}
+
+	int GetGameObjectID() const { return m_GameObjectID; }
+
+
+	void AddComponent(Component* comp)
+	{
+		m_Components.push_back(comp);
+	}
+
+	bool SendMessage(CoreMessage* msg)
+	{
+		bool isSent = false;
+
+		// Si on reçoit un message set ou get : deux cas à traiter 
+		switch (msg->m_messageType)
+		{
+		case SetPosition:
+		{
+			SetPositionMessage* msgSetPos = static_cast<SetPositionMessage*>(msg);
+			m_Position.x = msgSetPos->x;
+			m_Position.y = msgSetPos->y;
+			m_Position.z = msgSetPos->z;
+
+			isSent = true;
+			cout << "GameObject has SetPosition\n";
+		}
+		break;
+		case GetPosition:
+		{
+			GetPositionMessage* msgSetPos = static_cast<GetPositionMessage*>(msg);
+			msgSetPos->x = m_Position.x;
+			msgSetPos->y = m_Position.y;
+			msgSetPos->z = m_Position.z;
+
+			isSent = true;
+			cout << "GameObject is trying to GetPosition\n";
+		}
+		break;
+		default:
+			return PassMessageToComponents(msg);
+		}
+
+		// Si le message est traité par un composant on renvoie true
+		isSent = isSent | PassMessageToComponents(msg);
+
+		return isSent;
+	}
+
 };
 
 class Creator
@@ -51,10 +198,11 @@ class Creator
 class Factory
 {
 public:
-	GameObject* CreateObject(std::string type);
-	void Register(std::string type, Creator *creator);
-	std::map<std::string, Creator* > m_Factory;
-	std::map < std::string, std::vector<GameObject*> > m_Reserve;
+	GameObject* CreateObject(string type);
+	void Register(string type, Creator *creator);
+
+	map<string, Creator* > m_Factory;
+	map < string, vector<GameObject*> > m_Reserve;
 };
 
 
@@ -63,17 +211,27 @@ class GOIterator
 {
 	
 	int index;
-	std::vector<GameObject*> * objects;
+	int indexComponent;
+	vector<GameObject*> * objects;
 public:
 
-	std::mutex mutex;
+	mutex mutex;
 
-	GOIterator(std::vector<GameObject*>* obj) : objects(obj), mutex()
+	GOIterator(vector<GameObject*>* obj) : objects(obj), mutex()
 	{
 		index = 0;
 	}
 
 	GameObject* get()
+	{
+		if (finished())
+		{
+			return NULL;
+		}
+
+		return (*objects)[index];
+	}
+	Component* getComponent()
 	{
 		if (finished())
 		{
@@ -96,10 +254,10 @@ public:
 
 class Worker
 {
-	std::condition_variable condWork;
+	condition_variable condWork;
 	GOIterator *iterator;
-	std::atomic<bool> working;
-	std::thread thread;
+	atomic<bool> working;
+	thread thread;
 public: 
 	
 	Worker() : thread(&Worker::work, this)
@@ -134,19 +292,19 @@ public:
 		{
 			
 			iterator = NULL;
-			std::mutex mutex;	
+			mutex mutex;	
 			working.store(false);
 
 			while (!working)
 			{
-				condWork.wait_for(std::unique_lock<std::mutex>(mutex), std::chrono::milliseconds(10));
+				condWork.wait_for(unique_lock<mutex>(mutex), chrono::milliseconds(10));
 			}
 		
 			while (1)
 			{
 				GameObject* obj = NULL;
 				{
-					std::lock_guard<std::mutex> lock(iterator->mutex);
+					lock_guard<mutex> lock(iterator->mutex);
 
 					if (iterator->finished())
 					{
@@ -208,7 +366,7 @@ class GameEngine
 
 private:
 	Boss boss;
-	std::vector<GameObject*> objects;
+	vector<GameObject*> objects;
 public:
 
 	void AddObject(GameObject* obj)
